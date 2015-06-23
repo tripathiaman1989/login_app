c4398f2b17ee94c09fa413bb2b2494062fc564be
Aman <aman@sancustechnologies.com>
2015-06-23 22:25:37 +0530

    devise and facebook intregation

---
+++ db/migrate/20150621135534_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :link, :string
+  end
+end
